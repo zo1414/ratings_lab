47d4a19a822f33dd1b98412d0776e6fdfb2680e7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-19 13:00:25 -0700

    created server pages to register, login, logout

--- server.py
+++ server.py
@@ -34,6 +34,71 @@ def user_list():
     return render_template("user_list.html", users=users)
 
 
+@app.route("/register", methods=["GET"])
+def register_form():
+
+    pass
+
+    return render_template("register_form.html")
+
+
+@app.route("/register", methods=["POST"])
+def register_process():
+    """Check db for email."""
+
+    input_email = request.form.get("email")
+    input_password = request.form.get("password")
+
+    if not User.query.filter(User.email == input_email).all():
+
+        new_user = User(email=input_email, password=input_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Successfully registered")
+
+    else:
+        flash("User already registered")
+
+    #session["user"] = [input_email, input_password]
+    # print "added to session %s %s" % (input_email, input_password)
+    return redirect("/")
+
+@app.route("/login", methods=["GET"])
+def login_form():
+
+    return render_template("login_form.html")
+
+
+@app.route("/login", methods=["POST"])
+def login_process():
+    """log user into session"""
+
+    input_email = request.form.get("email")
+    input_password = request.form.get("password")
+
+    current_user = User.query.filter(User.email == input_email).first()
+
+    if "user" in session:
+        flash("%s is logged in." % (session["user"]))
+        return redirect("/")
+
+    if current_user is not None and current_user.password == input_password:
+        session["user"] = input_email
+        flash("Logged In")
+        return redirect("/")
+    else:
+        flash("Login failed")
+        return redirect("/login")
+
+@app.route("/logout")
+def logout_process():
+    """Logs user out by removing info from session."""
+
+    session.pop("user")
+    flash("Logged out")
+    return redirect("/")
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
@@ -44,7 +109,5 @@ if __name__ == "__main__":
 
     # Use the DebugToolbar
     DebugToolbarExtension(app)
-
-
 
     app.run(port=5000, host='0.0.0.0')
